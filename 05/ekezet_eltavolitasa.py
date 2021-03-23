e757204d4a1d7b25600f84b5bbcf51553f0b0a45
Nagy Lajos <nagylali001@gmail.com>
2021-03-23 17:18:03 +0100

    ekezet_eltavolitasa.py extended with dictionary

--- 05/ekezet_eltavolitasa.py
+++ 05/ekezet_eltavolitasa.py
@@ -11,14 +11,26 @@ A döntés várhatóan nem marad politikai visszhang nélkül Spanyolországban,
 rovid = "aeiooouuuAEIOOOUUU"
 hosszu = "áéíóöőúüűÁÉÍÓÖŐÚÜŰ"
 
+d = {
+    'á':'a', 'é':'e', 'í':'i', 'ó':'o', 'ö':'o', 'ő':'o',
+    'ú':'u', 'ü':'u', 'ű':'u', 'Á':'A', 'É':'E', 'Í':'I',
+    'Ó':'O', 'Ö':'O', 'Ő':'O', 'Ú':'U', 'Ü':'U', 'Ű':'U'
+    }
 
 def main():
+    print("\n============Replace without dictionary============")
     for c in TEXT:
         pos = hosszu.find(c)
         if pos >= 0:
             print(rovid[pos], end = "")
         else:
             print(c, end = "")
+    
+    print("\n============Replace with dictionary===============")
+    new_text = TEXT
+    for k, v in d.items():
+        new_text = new_text.replace(k, v)
+    print(new_text)
 
 #############################################################################
 
