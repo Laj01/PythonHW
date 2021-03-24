9dae3ab38c9b74fb4217bf93a1e076e7d8255488
Nagy Lajos <nagylali001@gmail.com>
2021-03-24 12:26:57 +0100

    median.py format fix

--- 05/median.py
+++ 05/median.py
@@ -2,7 +2,6 @@
 
 
 def median(list_of_numbers):
-
     list_of_numbers = sorted(list_of_numbers)
     
     if len(list_of_numbers) % 2 == 1:
@@ -14,15 +13,14 @@ def median(list_of_numbers):
 
 
 def main():
-
     list1 = [1, 2, 3, 4, 5]
     list2 = [3, 1, 2, 5, 3]
     list3 = [1, 300, 2, 200, 1]
     list4 = [3, 6, 20, 99, 10, 15]
-    print(f"Median for list1({list1}) == {median(list1)}")
-    print(f"Median for list2({list2}) == {median(list2)}")
-    print(f"Median for list3({list3}) == {median(list3)}")
-    print(f"Median for list4({list4}) == {median(list4)}")
+    print(f"Median for ({list1}) == {median(list1)}")
+    print(f"Median for ({list2}) == {median(list2)}")
+    print(f"Median for ({list3}) == {median(list3)}")
+    print(f"Median for ({list4}) == {median(list4)}")
 
 #############################################################################
 
